5c4cb7efc985a35cdafcedcf4fd88898d0646507
maticufre <matiascufret@gmail.com>
2026-03-18 02:24:27 -0300

    Terminado

--- js/main.js
+++ js/main.js
@@ -3,20 +3,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Sistema inicializado: Portafolio de Matías Cufre cargado.");
 
-    // --- MODO OSCURO (Preparación) ---
     const btnDarkMode = document.getElementById('btn-dark-mode');
-    if (btnDarkMode) {
-        btnDarkMode.addEventListener('click', () => {
-            console.log("Próximamente: Lógica de Modo Oscuro");
-            // Aquí agregaremos la clase 'dark' al HTML
-        });
+    const icon = btnDarkMode.querySelector('i');
+    const htmlElement = document.documentElement;
+
+    // 1. Revisar si el usuario ya tenía el modo oscuro activado
+    if (localStorage.getItem('theme') === 'dark') {
+        htmlElement.classList.add('dark-theme');
+        icon.className = 'fa-regular fa-sun text-lg'; 
     }
 
-    // --- TRADUCCIÓN (Preparación) ---
-    const btnLang = document.getElementById('btn-language');
-    if (btnLang) {
-        btnLang.addEventListener('click', () => {
-            console.log("Próximamente: Lógica de Traducción a Inglés");
+    // 2. Lógica al hacer clic en el botón
+    if (btnDarkMode) {
+        btnDarkMode.addEventListener('click', () => {
+            htmlElement.classList.toggle('dark-theme');
+
+            if (htmlElement.classList.contains('dark-theme')) {
+                localStorage.setItem('theme', 'dark');
+                // Cambiar al sol
+                icon.className = 'fa-regular fa-sun text-lg';
+            } else {
+                localStorage.setItem('theme', 'light');
+                // Volver a la luna
+                icon.className = 'fa-regular fa-moon text-lg';
+            }
         });
     }
 });
